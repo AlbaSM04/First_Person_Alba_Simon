4b4b301d61c2d513daa052f8601793495ba7359a
AlbaSM04 <alba.simon.mendez@gmail.com>
2024-10-17 21:14:36 +0200

    17/10/2024

--- Assets/Script/FistPerson.cs
+++ Assets/Script/FistPerson.cs
@@ -11,6 +11,7 @@ public class FistPerson : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
